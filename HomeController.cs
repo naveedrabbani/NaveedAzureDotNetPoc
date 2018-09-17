c7b03dba1df6d48124cb0adefe594b77b1ef9e94
naveedrabbani <43229052+naveedrabbani@users.noreply.github.com>
2018-09-16 22:34:15 -0500

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using NaveedDotNetPoc.Models;
+
+namespace NaveedDotNetPoc.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private IConfiguration _configuration;
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("api/v0/myrestendpoint")]
+        [HttpGet]
+        public IActionResult GetApplicationConfigurationDetails()
+        {
+            var analyticsUrl = _configuration["AnalyticsUrl"];
+            return Ok($"The Analytics Url is {analyticsUrl}");
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
